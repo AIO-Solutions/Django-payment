ec583f1a2eb2b3009bb143c7092239347957e100
umidyor asomov <106219812+umidyor@users.noreply.github.com>
2024-02-07 17:51:22 +0500

    simple

---
+++ README.md
@@ -0,0 +1,9 @@
+<h3>Firstly full signup <a href="https://stripe.com/"><i>stripe.com site</i></a> </h3>
+
+<h3>And get 2 keys from developers dashboard in site.</h3>
+
+```commandline
+STRIPE_SECRET_KEY=' '
+STRIPE_PUBLISHABLE_KEY=' '
+```
+<h1>Please firstly learn the stripe site docs...</h1>
---
+++ payments/views.py
@@ -0,0 +1,25 @@
+import stripe
+from django.shortcuts import render
+from django.conf import settings
+import stripe
+stripe.api_key=settings.STRIPE_SECRET_KEY
+# Create your views here.
+from django.views.generic.base import TemplateView
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['key']=settings.STRIPE_PUBLISHABLE_KEY
+        print(context['key'])
+        return context
+
+def charge(request):
+    if request.method=="POST":
+        charge=stripe.Charge.create(
+            amount=500,
+            currency='usd',
+            description='Payment Gateway',
+            source=request.POST['stripeToken']
+        )
+    return render(request,'charge.html')
